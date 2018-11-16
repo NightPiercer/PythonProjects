277f996e0281df340513ace9fda89dd62fa34103
Alex Horn <alexhorn@mail.weber.edu>
2018-11-15 18:55:41 -0700

    finished variable examples

---
+++ .vscode/variables.py
@@ -0,0 +1,87 @@
+# Variables
+#   Data Types
+#       Dynamic types
+#       type: int, float, string, boolean
+
+#   type variable = value
+#   int age = 38 --> integer
+#   Reason for Datatype
+#       allocation of space --> Age would take up 2 spaces  exceed 2 spaces error -- error handling --- amount of memory used
+#       error handling --> exceptions  age = five or age = blue
+
+# age = 23
+# print (age)
+# age = "twenty three" # reassignment of a variable
+# print (age)
+
+
+# nameOne = "Scott"
+# nameTwo = nameOne
+
+# print(' The first name ' + nameOne + ' the second name ' + nameTwo)
+
+# print(id(nameOne))
+# print(id(nameTwo))
+# nameOne ="John"
+# print(' The first name ' + nameOne + ' the second name ' + nameTwo)
+
+# print(id(nameOne))
+# print(id(nameTwo))
+
+# nameTwo = "Bill"
+# print("Bill 1")
+# print(id(nameOne))
+# print(id(nameTwo))
+
+# nameTwo = "Bill"
+# print("Bill 2")
+# print(id(nameOne))
+# print(id(nameTwo))
+
+# Limited use variable names
+#  yield = "yes"      ----> Reserved Words not allowed
+# 3dVariable = "cube" ----> Starting with a number
+# $testvariable = "$56" --> Starting with a symbol
+
+#variable notation
+# the_underscore_variable = "recommended by Python style guide"
+# camelCaseVariable = "recommended by Scott"
+# NEVER_GOING_TO_CHANGE_VARIABLE = "Scott"
+
+# Numbers and variables
+# hexadecimalNumber = hex(34)
+# print(hexadecimalNumber)
+
+# binaryNumber = bin(34)
+# print(binaryNumber)
+
+# bigNumber = 99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
+# print (bigNumber)
+
+# Strings
+
+# String assignment and position
+# myFirstString = "Hello World"
+# print(myFirstString)
+# print(myFirstString[0])
+# print(len(myFirstString))
+
+#string comparison
+# class1 = "CS 1030"
+# class2 = "CS 1030"
+# class3 = "CS 1400"
+
+# print(class1 == class2)
+# print(class2 == class3)
+
+# floatNumber = 3.09
+# intNumber = 3
+
+# sumnumber = floatNumber + intNumber
+# print (sumnumber)
+# print (intNumber)
+
+# Boolean Variable
+
+# isLightOn = True
+# isLightOff = False
